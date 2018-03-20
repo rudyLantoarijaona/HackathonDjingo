928253114c9875492695a93c6829708976da357d
rudyLantoarijaona <rudy.lantoarijaona@gmail.com>
2018-03-20 22:13:38 +0100

    Add ChatBot

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  token: 'bbd96fbbe3994cfa8964a5070245a856'
 };
